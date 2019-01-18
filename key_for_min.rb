3f7d2c31dfdcbdcd0281fdbf5e90f34ef757a1ae
Vinh Do <vinhdo07@gmail.com>
2019-01-18 22:05:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_array = []
+  name_hash.each_value do |value|
+    min_array << value
+  end
+  
+    
 end
